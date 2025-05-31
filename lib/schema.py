d132427b7ede0a6bc22c0fc1b84611894e5c3152
Yazz <itsjustmaitai@gmail.com>
2025-05-31 14:26:26 +0300

    CREATED 3 tables

---
+++ lib/schema.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+conn=sqlite3 .connect('book.db')
+cursor=conn.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXIST Author(
+               id INTERGER PRIMARY KEY,name TEXT,birth_year INTERGER
+               )
+
+            ''')
+cursor.execute('''
+        CREATE TABLE IF NOT EXIST Genre(
+               id INTERGER PRIMARY KEY,
+               name TEXT UNIQUE NOT NULL
+
+               )
+            ''')
+cursor.execute('''
+        CREATE TABLE IF NOT EXIST Books(
+               id INTEGER PRIMARY KEY,
+               title TEXT NOT NULL,
+               FOREIGN KEY(author_id) REFERENCE Author(id),
+               FOREIGN KEY(genre_id) REFERENCE Genre(id),
+               year_published INTERGER
+               )          
+
+                ''')
+conn.commit()
